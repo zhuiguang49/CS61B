0f7c6c7e08f81aa59b1e00267abe5d221dc45cee
zhuiguang <3230105121@zju.edu.cn>
2025-07-24 18:37:19 +0800

    finish project0(game2048)

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 # CS61B
 > My note of CS61B is available at the website https://note.zhuiguang.tech
 
+## Project
+
+### Project0:2048
+
+Project0 以 2048 游戏为背景，希望我们完成 `emptySpaceExists`、`makeTileExists`、`atLeastOneMoveExists` 和 `tilt` 四个方法
+
+#### `emptySpaceExists` 方法
+
+该方法是要检查棋盘中是否存在为 `null` 的格子，只要注意一下 `value` 是 `private` 的，不能直接访问，需要用 `tile` 函数间接访问即可，实现用一个很简单的两层循环就行
+
+#### `maxTileExists` 方法
+
+`maxTileExists` 方法是要检查棋盘上是否有值等于 `MAX_PIECE` 的方块，检查的循环逻辑和 `emptySpaceExists` 方法类似，但是要注意调用 `b.tile(row,col).value()` 的时候需要先检查 `b.tile(row,col)` 是否为 `null`，如果是 `null`，就会抛出 `NullPointerException` 的异常，导致 `TestMaxTileExists.java` 测试不通过
+
+#### `atLeastOneMoveExist` 方法
+
+`atLeastOneMoveExist` 的判定比较容易理解，两个条件：
+
+1. 棋盘上还有空格
+2. 相邻方块有相同值
+
+棋盘上还有空格通过 `emptySpaceExists` 方法就能很容易地判定，但是第二个条件看起来似乎没有那么好实现，我自己的做法是定义了一个 `checkvalue` 函数，其函数原型如下：
+
+```java
+public static boolean checkvalue(int row,int col,Board b)
+```
+
+用于检查相邻的方块是否有相同值，从而简化 `atLeastOneMoveExist` 函数的实现，但实际上 `checkvalue` 函数的实现仍然是非常繁琐的。在 `proj0/game2048/Model.java` 的注释里我贴了一个 AI 实现的 `atLeastOneMoveExist` 函数，实现非常的简洁，主要逻辑是：
+- 从左上角开始遍历棋盘(`row` 从 0 到 `size-1`，`col` 从 0 到 `size -1`)
+- 对于每一个非空的 `Tile`，只检查它右边和下边的相邻格子
+
+这样检查的逻辑比我们想象的简单很多，这是因为当我们检查当前格子的右边格子时，右边格子的左边就是当前格子，所以在后面循环到“右边格子”时我们不再需要检查它的左边格子；下边格子也是同理，因此我们只需要检查右边和下面的格子就可以，这样的逻辑是简单的。
+
+#### `tilt`方法
+
+>这里 Tips 中有这么一段话 "In a given column, the piece on the top row (row 3) stays put. The piece on row 2 can move up if the space above it is empty, or it can move up one if the space above it has the same value as itself. In other words, when iterating over rows, it is safe to iterate starting from row 3 down, since there’s no way a tile will have to move again after moving once."。需要非常注意的是，top row 被标记为了 row 3 (也即 board.size()-1)，这和我们平常的规定是不同的，如果没注意的话后面的理解、实现会有比较大的偏差，可以自行对照 `TestUpOnly` 查看预期结果和实际结果 debug。
+
+**视角朝北的实现：**
+
+我的实现是这样的，CS61B 的文档给出了部分提示，我们可以从第二行（实际的行坐标是 `board.size()-2`）向下进行遍历，然后对每一个 tile 考虑它的 `move`。对于当前的 tile `tile(col,row)` 大致可以分为以下三种情况：
+- 该列上方所有行都为 null，只需直接移动到该列的第一行即可，也即 `board.move(col,board.size()-1,tile(col,row))`
+- 该列上方有不为 null 的 tile（我们需要找到离当前 tile 最近的一个，我们记其横坐标为 `destination`），两者的 value 并不相等，这时候需要移动到它的下方，也即 `board.move(col,destination-1,tile(col,row))`
+- 该列上方有不为 null 的 tile（也要找到离当前 tile 最近的一个，我们同样记其横坐标为 `destination`），且其 value 与
+`tile(col,row).value` 相同，**这时候我们就需要考虑 `(col,destination)` 这个位置是否合并过，因为题目明确指出只能合并一次**。我的实现是采用了一个 boolean 类型的二维数组 mergedArrays，默认值为 false，用于标记当前位置是否合并。如果值为 false，那就移动到 `(col,destination)` 的位置，并且更新分数；如果值为 true，那就移动到 `(col,destination-1)` 的位置。
+
+实现完视角朝北，对于其他值的 `side`，我们只需要仿照项目文档给出的，在最开始的时候设置 `board.setViewingPerspective(Side.blabla)`，然后在最后设置 `board.setViewingPerspective(Side.NORTH)` 即可
+
+## Lab
+
--- skeleton-sp21/proj0/game2048/Model.java
+++ skeleton-sp21/proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author zhuiguang
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -109,11 +109,76 @@ public class Model extends Observable {
     public boolean tilt(Side side) {
         boolean changed;
         changed = false;
-
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
 
+        // 如果视角不是朝北，就设置视角朝北以简化实现
+        if(side == Side.WEST){
+            board.setViewingPerspective(Side.WEST);
+        }else if(side == Side.EAST){
+            board.setViewingPerspective(Side.EAST);
+        }else if(side == Side.SOUTH){
+            board.setViewingPerspective(Side.SOUTH);
+        }
+
+        boolean[][] mergedArrays= new boolean[board.size()][board.size()];
+
+        for(int col = 0; col < board.size(); col++){
+            for(int row = board.size()-2; row >= 0; row--){
+                if(tile(col,row)==null){
+                    continue;
+                }
+                // 这里我大致把 board 上面的每个格子分成了三类情况
+                // 第一种，上方所有格子内没有 tile
+                // 第二种，上方格子有 tile，但是值不相等
+                // 第三种，上方格子有 tile，且值相等，这时候要依靠 mergedArrays 来区分是否合并（也即移动到当前格子还是移动到下面的格子）
+
+                boolean allNull = true; // 判断是否均为 null
+
+                // 循环遍历 (row,col) 位置元素上方的所有 tile，判断是否均为 null
+                for(int it = board.size()-1; it > row; it--){
+
+                    // 如果有非 null 的
+                    if(tile(col,it) != null){
+                        allNull = false;
+                        break;
+                    }
+                }
+                if(allNull){ // 均为 null，直接将当前 tile 移动到 该列的第一行，并且将 changed 标记为 true
+                    board.move(col, board.size()-1,tile(col,row));
+                    changed = true;
+                }else{ // 上方有 tile 非 null 的情形
+                    int destination= board.size()-1;
+                    // 遍历上方所有行，找出离 (col,row) 最近的非 null 的 tile
+                    for(int it = board.size()-1; it > row; it--){
+                        if(tile(col,it) != null){
+                            destination = it;
+                        }
+                    }
+                    if(tile(col,destination).value() != tile(col,row).value()){ // 非 null 但是值不相等，移动到其下方
+                        board.move(col,destination-1,tile(col,row));
+                        changed = true;
+                    }else{ // 非 null 且值相等，分两类情况考虑，当前区域是否进行过合并，也即 mergedArrays 的值是 true 还是 false
+                        if(mergedArrays[col][destination] == false){ // 当前区域未合并
+                            changed = true;
+                            score = score + 2 * tile(col,row).value();
+                            board.move(col,destination,tile(col,row));
+                            mergedArrays[col][destination] = true;
+                        }else{ // 当前区域已经合并
+                            changed = true;
+                            board.move(col,destination-1,tile(col,row));
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        board.setViewingPerspective(Side.NORTH);
         checkGameOver();
         if (changed) {
             setChanged();
@@ -138,8 +203,16 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for(int i = 0; i < b.size(); i++){
+            for(int j = 0; j < b.size(); j++){
+                if(b.tile(j,i) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
+
 
     /**
      * Returns true if any tile is equal to the maximum valid value.
@@ -148,6 +221,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for(int row = 0; row < b.size(); row++){
+            for(int col = 0; col < b.size(); col++){
+                if(b.tile(col,row) != null && (b.tile(col,row)).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -157,11 +237,122 @@ public class Model extends Observable {
      * 1. There is at least one empty space on the board.
      * 2. There are two adjacent tiles with the same value.
      */
+    public static boolean checkvalue(int col, int row, Board b) {
+        if (col == 0) {
+            if (row == 0) {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value()) {
+                    return true;
+                }
+            } else if (row == b.size() - 1) {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            } else {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            }
+        } else if (col == b.size() - 1) {
+            if (row == 0) {
+                if (b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value()) {
+                    return true;
+                }
+            } else if (row == b.size() - 1) {
+                if (b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            } else {
+                if (b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            }
+        } else {
+            if (row == 0) {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value()) {
+                    return true;
+                }
+            } else if (row == b.size() - 1) {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            } else {
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col - 1, row).value() ||
+                        b.tile(col, row).value() == b.tile(col, row + 1).value() ||
+                        b.tile(col, row).value() == b.tile(col, row - 1).value()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        if(emptySpaceExists(b)){
+            return true;
+        }else{
+            for(int row = 0; row < b.size(); row++){
+                for(int col = 0; col < b.size(); col++){
+                    if(checkvalue(col,row,b)){
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+    }
+    /*
+     这里我的实现写的比较繁琐，我是自己定义了一个 checkvalue 函数 `public static boolean checkvalue(int row,int col,Board b)`
+     （不过这个实现实在是太臭了），用于检查第二条要求 'There are two adjacent tiles with the same value'，
+     然后 `atLeastOneMoveExists` 函数使用 `checkvalue`函数实现，实现逻辑看起来就没那么繁琐
+     */
+
+    // 下面是 AI 提供的一个更简便的思路，其大概的思路是由于我们迭代是从左上方开始迭代的，所以我们检查的时候其实只需要检查右下就可以了，事实上，后面的
+    // 方块的左上其实在前面就已经检查过了，所以逻辑就没有我们想象中的那么繁琐。
+    /*
+    public static boolean atLeastOneMoveExists(Board b) {
+        int size = b.size();
+
+        // 如果存在空位，直接返回 true
+        if (emptySpaceExists(b)) {
+            return true;
+        }
+
+        // 检查是否有相邻且值相同的 tile
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                Tile current = b.tile(row, col);
+
+                // 向右和向下检查，避免重复检查
+                if (col + 1 < size && b.tile(col + 1, row) != null &&
+                        current.value() == b.tile(col + 1, row).value()) {
+                    return true;
+                }
+
+                if (row + 1 < size && b.tile(col, row + 1) != null &&
+                        current.value() == b.tile(col, row + 1).value()) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
+*/
 
     @Override
      /** Returns the model as a string, used for debugging. */
